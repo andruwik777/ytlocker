112e49be34b96632785cbb851d31d62046454b73
petrov <andruwik777@gmail.com>
2018-04-22 02:52:39 +0300

    added PoC with password lock

--- app/src/main/java/org/ecloga/ytlocker/MainService.java
+++ app/src/main/java/org/ecloga/ytlocker/MainService.java
@@ -2,30 +2,24 @@ package org.ecloga.ytlocker;
 
 import android.app.Service;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.PixelFormat;
-import android.graphics.Typeface;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.support.annotation.Nullable;
-import android.util.DisplayMetrics;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
-import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainService extends Service {
 
-    private ImageView black;
-    private TextView tvInfo;
+    public static final int PASSWORD_RESET_DELAY_MILLIS = 1000;
     private WindowManager windowManager;
-    private FrameLayout layout;
+    private View passwordLayout;
 
     @Nullable
     @Override
@@ -39,31 +33,44 @@ public class MainService extends Service {
 
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
 
-        layout = new FrameLayout(this);
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        assert layoutInflater != null;
+        passwordLayout = layoutInflater.inflate(R.layout.password_layout, null);
 
-        FrameLayout.LayoutParams flParams = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                Gravity.CENTER);
+        TextView passwordTextView = passwordLayout.findViewById(R.id.textView);
+        passwordTextView.addTextChangedListener(new TextWatcher() {
+            Handler handler = new Handler(Looper.getMainLooper());
 
-        black = new ImageView(this);
-        black.setImageResource(R.drawable.black);
+            @Override
+            public void afterTextChanged(Editable s) {
+                handler.removeCallbacksAndMessages(null);
+                if (isPasswordCorrect(s.toString())) {
+                    stopSelf();
+                } else {
+                    if (!passwordTextView.getText().toString().isEmpty()) {
+                        handler.postDelayed(() -> passwordTextView.setText(""), PASSWORD_RESET_DELAY_MILLIS);
+                    }
+                }
+            }
 
-        DisplayMetrics metrics;
-        metrics = getApplicationContext().getResources().getDisplayMetrics();
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
-        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/timeburnernormal.ttf");
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+        });
 
-        tvInfo = new TextView(this);
-        tvInfo.setText("Hold to unlock");
-        tvInfo.setTextColor(Color.parseColor("#ecf0f1"));
-        tvInfo.setTypeface(font);
-
-        float Textsize = tvInfo.getTextSize() / metrics.density;
-        tvInfo.setTextSize(Textsize + 15);
-
-        layout.addView(black);
-        layout.addView(tvInfo, flParams);
+        changePasswordWhenButtonClicked(R.id.button1, passwordTextView, "1");
+        changePasswordWhenButtonClicked(R.id.button2, passwordTextView, "2");
+        changePasswordWhenButtonClicked(R.id.button3, passwordTextView, "3");
+        changePasswordWhenButtonClicked(R.id.button4, passwordTextView, "4");
+        changePasswordWhenButtonClicked(R.id.button5, passwordTextView, "5");
+        changePasswordWhenButtonClicked(R.id.button6, passwordTextView, "6");
+        changePasswordWhenButtonClicked(R.id.button7, passwordTextView, "7");
+        changePasswordWhenButtonClicked(R.id.button8, passwordTextView, "8");
+        changePasswordWhenButtonClicked(R.id.button9, passwordTextView, "9");
 
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
@@ -78,46 +85,20 @@ public class MainService extends Service {
         params.x = 0;
         params.y = 0;
 
-        windowManager.addView(layout, params);
+        windowManager.addView(passwordLayout, params);
+    }
 
-        Animation fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
-        black.startAnimation(fadeIn);
+    private void changePasswordWhenButtonClicked(int buttonId, TextView password, String nextPasswordChar) {
+        passwordLayout.findViewById(buttonId).setOnClickListener(view -> password.setText(getString(R.string.passwordValue, password.getText(), nextPasswordChar)));
+    }
 
-        black.getLayoutParams().height = windowManager.getDefaultDisplay().getHeight();
-        black.getLayoutParams().width = windowManager.getDefaultDisplay().getWidth();
-
-        black.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Handler handler = new Handler();
-
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        stopSelf();
-                    }
-                }, 500);
-
-                return false;
-            }
-        });
-
-        black.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (event.getAction() == KeyEvent.KEYCODE_VOLUME_DOWN) {
-                    Toast.makeText(getApplicationContext(), "asd", Toast.LENGTH_LONG).show();
-                }
-
-                return false;
-            }
-        });
+    private boolean isPasswordCorrect(String password) {
+        return "123".equals(password);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-
-        windowManager.removeView(layout);
+        windowManager.removeView(passwordLayout);
     }
 }
